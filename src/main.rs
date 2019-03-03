cf76d12805f1d85a43a7038c87e24dd9e16c7be9
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2019-03-03 15:21:34 +0100

    feature: initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world from GAE!"
+}
+
+fn main() {
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("0.0.0.0:8080")
+        .unwrap()
+        .run();
+}
